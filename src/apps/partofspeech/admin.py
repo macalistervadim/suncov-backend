321215d3fba3f7a0cc4a489fc29f27687c93628d
macalistervadim <macalistervadim@yandex.ru>
2025-02-20 18:38:58 +0600

    add short_text def in admin.py pertofspeech

--- src/apps/partofspeech/admin.py
+++ src/apps/partofspeech/admin.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.contrib import admin
 
 from src.apps.partofspeech.models.part_of_speech import PartOfSpeech, Theme
@@ -5,11 +7,16 @@ from src.apps.partofspeech.models.part_of_speech import PartOfSpeech, Theme
 
 @admin.register(PartOfSpeech)
 class PartOfSpeechAdmin(admin.ModelAdmin):
-    list_display = ("theme__title",)
+    list_display = ("theme__title", "short_text")
     search_fields = ("theme__title",)
     list_filter = ("theme__title",)
     ordering = ("-theme__title",)
     list_per_page = 20
+
+    def short_text(self, obj: Any) -> str:
+        return f"{obj.text[:50]}…" if len(obj.text) > 50 else obj.text
+
+setattr(PartOfSpeechAdmin.short_text, "short_description", "Text")
 
 
 @admin.register(Theme)
